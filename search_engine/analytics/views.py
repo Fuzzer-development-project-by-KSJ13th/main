04338b9569d0af6d4927746403379af14e353f79
byObin <byobin26@gmail.com>
2024-09-25 16:37:13 +0900

    Feat[#1] 기능별 메인 화면 구성

--- search_engine/analytics/views.py
+++ search_engine/analytics/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Analytics
 
-# Create your views here.
+def analytics_home(request):
+    analytics = Analytics.objects
+    return render(request, 'analytics_home.html', {'analytics':analytics})
--- search_engine/api_search/views.py
+++ search_engine/api_search/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Search
 
-# Create your views here.
+def search_home(request):
+    searches = Search.objects
+    return render(request, 'search_home.html', {'searches':searches})
