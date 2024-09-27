b78092400ad01437d24a4d558396fd1232b91b10
byObin <byobin26@gmail.com>
2024-09-27 16:28:56 +0900

    Feat 분석 cve, cvss 모두 저장

--- search_engine/analytics/views.py
+++ search_engine/analytics/views.py
@@ -10,16 +10,19 @@ def analytics_home(request):
 
 
 def get_data(request):
-    response = requests.get('https://cvedb.shodan.io/cves?cpe23=cpe%3A2.3%3Aa%3Apalletsprojects%3Aflask%3A1.1.4&count=false&is_kev=false&sort_by_epss=false&skip=0&limit=1000')
+    response = requests.get('https://cvedb.shodan.io/cves?product=flask&count=false&is_kev=false&sort_by_epss=false&skip=0&limit=1000')
     
     if response.status_code == 200:
         api_data = response.json()
-        cve_num = api_data['cves'][0]['cve_id']
-        cvss_val = api_data['cves'][0]['cvss']
-        data = {
-            'cve_id' : [cve_num],
-            'cvss_value' : [cvss_val]
 
-        }
-        
+        data = {'cve_id':[], 'cvss_value':[]}
+
+        for i in range(len(api_data['cves'])):
+            cve_num = api_data['cves'][i]['cve_id']
+            cvss_val = api_data['cves'][i]['cvss']
+            
+            data['cve_id'].append(cve_num)
+            data['cvss_value'].append(cvss_val)
+        print(data)
+
     return JsonResponse(data)
