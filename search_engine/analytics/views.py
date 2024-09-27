c4e828ae68f8ff80fe25a81a223e1d403de07b51
byObin <byobin26@gmail.com>
2024-09-27 14:55:09 +0900

    Feat 분석 화면 차트 출력

--- search_engine/analytics/views.py
+++ search_engine/analytics/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from .models import Analytics
+from django.http import JsonResponse
+import requests
+
 
 def analytics_home(request):
     analytics = Analytics.objects
     return render(request, 'analytics_home.html', {'analytics':analytics})
+
+
+def get_data(request):
+    response = requests.get('https://cvedb.shodan.io/cves?cpe23=cpe%3A2.3%3Aa%3Apalletsprojects%3Aflask%3A1.1.4&count=false&is_kev=false&sort_by_epss=false&skip=0&limit=1000')
+    
+    if response.status_code == 200:
+        api_data = response.json()
+        cve_num = api_data['cves'][0]['cve_id']
+        cvss_val = api_data['cves'][0]['cvss']
+        data = {
+            'cve_id' : [cve_num],
+            'cvss_value' : [cvss_val]
+
+        }
+        
+    return JsonResponse(data)
--- search_engine/search_engine/urls.py
+++ search_engine/search_engine/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('search/', api_search.views.search_home, name='search_home'),
     path('analytics/', analytics.views.analytics_home, name='analytics_home'),
+    path('cvss-data/', analytics.views.get_data, name='get-data'),
+
 ]
