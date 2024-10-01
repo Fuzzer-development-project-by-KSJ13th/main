4b642c42bada1966d207d840326b9af0ffa8878a
soo-minn <139030000+soo-minn@users.noreply.github.com>
2024-10-01 09:43:43 +0900

    Add files via upload

--- search_engine/search_engine/urls.py
+++ search_engine/search_engine/urls.py
@@ -3,10 +3,13 @@ from django.urls import path
 import home.views
 import api_search.views
 import analytics.views
+import analytics.views
 
 urlpatterns = [
     path('', home.views.home),
     path('admin/', admin.site.urls),
     path('search/', api_search.views.search_home, name='search_home'),
     path('analytics/', analytics.views.analytics_home, name='analytics_home'),
+    path('cvss-data/', analytics.views.get_data, name='get-data'),
 ]
+
