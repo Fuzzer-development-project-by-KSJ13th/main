b44323d6ed312afbc23f2bab7b2addf533a05318
huizl <65006404+liu5643@users.noreply.github.com>
2024-10-07 13:40:47 +0900

    Add files via upload

--- search_engine/accounts/models.py
+++ search_engine/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=30, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    job = models.CharField(max_length=100, blank=True)
+    
+    def __str__(self):
+        return self.user.username
