146beedac9bc94f35876b22bca3ee99ed185bc3e
soo-minn <139030000+soo-minn@users.noreply.github.com>
2024-10-07 17:26:58 +0900

    models.py

--- search_engine/api_search/models.py
+++ search_engine/api_search/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 class Search(models.Model):
     keyword = models.TextField()
+
+class SearchHistory(models.Model):
+    product_name = models.CharField(max_length=255)  # 검색한 제품명
+    cpe_result = models.TextField()  # CPE 검색 결과 (JSON 형태로 저장 가능)
+    search_date = models.DateTimeField(default=timezone.now)  # 검색한 시간
+    username = models.CharField(max_length=150, null=True, blank=True)
+
+    def __str__(self):
+        return self.product_name
