9411a1e7bd980d0caff84fa1f7b39dd242a4b493
byObin <byobin26@gmail.com>
2024-10-07 15:47:55 +0900

    Feat[#6] radio 버튼 생성 및 선택 값 전달

--- search_engine/api_search/forms.py
+++ search_engine/api_search/forms.py
@@ -3,3 +3,14 @@ from django import forms
 
 class NameForm(forms.Form):
     name = forms.CharField(label='제품 이름', max_length=100)
+
+    SEARCH_CHOICES = [
+        ('shodan', 'shodan'),
+        ('nvd', 'nvd')
+    ]
+    
+    search_option = forms.ChoiceField(
+        choices=SEARCH_CHOICES,  
+        widget=forms.RadioSelect,  
+        label="Select a search option",  # 라벨
+    )
